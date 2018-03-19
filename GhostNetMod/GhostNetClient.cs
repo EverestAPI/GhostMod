c782851bdcd9b72de9317b59c89f891879a4c587
Maik Macho <nuda1998@gmail.com>
2018-03-19 23:52:48 +0100

    Cleanup before TP

--- GhostNetMod/GhostNetClient.cs
+++ GhostNetMod/GhostNetClient.cs
@@ -719,6 +719,7 @@ namespace Celeste.Mod.Ghost.Net {
                             Session.FirstLevel = false;
                         }
                         LevelEnterExt.ErrorMessage = null;
+                        Cleanup();
                         Engine.Scene = new LevelLoader(Session, frame.UUpdate?.Data.Position);
 
                     } else {
